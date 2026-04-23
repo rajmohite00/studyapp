98e7da741a9cbc86c928c53eac9e926db3a009a2
raj mohite <madhukarmohite6@gmail.com>
2026-04-23 14:20:55 +0530

    normalfix

--- Frontend/lib/screens/profile_setup_screen.dart
+++ Frontend/lib/screens/profile_setup_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import '../app_theme.dart';
+import '../providers/auth_provider.dart';
 import '../widgets/primary_button.dart';
 
 class ProfileSetupScreen extends ConsumerStatefulWidget {
